a6ba06b8bacd5f46f21bd8a6d942892348e2813a
leonardo15043 <lahernandez15043@gmail.com>
2019-02-06 20:08:59 -0500

    Paginas base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,19 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { AjustesPage , Ajustes2Page, ModalPage, Pagina2Page, Pagina3Page, TabsPage } from '../pages/index.paginas';
+
+
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    AjustesPage,
+    Ajustes2Page,
+    ModalPage,
+    Pagina2Page,
+    Pagina3Page,
+    TabsPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +28,13 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    AjustesPage,
+    Ajustes2Page,
+    ModalPage,
+    Pagina2Page,
+    Pagina3Page,
+    TabsPage
   ],
   providers: [
     StatusBar,
